28c7595f9a53a05b255a3ade322f4342e7d1ae8b
alonsolopezjeronimo66-cpu <alonsolopezjeronimo66@gmail.com>
2025-11-06 13:11:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -40,10 +40,10 @@ if img_file_buffer is not None:
     # run the inference
     prediction = model.predict(data)
     print(prediction)
-    if prediction[0][0]>0.5:
-      st.header('Jeronimo, con Probabilidad: '+str( prediction[0][0]) )
-    if prediction[0][1]>0.5:
-      st.header('David, con Probabilidad: '+str( prediction[0][1]))
+    if prediction[0][0]>0.7:
+      st.header('Registro de rostro con exito,Bienvenido Jeronimo,: '+str( prediction[0][0]) )
+    if prediction[0][1]>0.7:
+      st.header('Registro de rostro con exito,Bienvenido David, con Probabilidad: '+str( prediction[0][1]))
     #if prediction[0][2]>0.5:
     # st.header('Derecha, con Probabilidad: '+str( prediction[0][2]))
 
